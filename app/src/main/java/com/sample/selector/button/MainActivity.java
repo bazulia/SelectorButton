fb62d9a36446eda642ef3d24b2f7d1c01a9d600f
IgorAndrDev <bazulia08@ukr.net>
2018-06-12 15:37:29 +0300

    edit readme.md

--- app/src/main/java/com/sample/selector/button/MainActivity.java
+++ app/src/main/java/com/sample/selector/button/MainActivity.java
@@ -3,10 +3,17 @@ package com.sample.selector.button;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.selector.button.SelectorButton;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SelectorButton selectorButton = (SelectorButton) findViewById(R.id.button);
+        selectorButton.setStateNormalDrawable(getResources().getDrawable(R.drawable.btn_normal));
+        selectorButton.setStatePressedDrawable(getResources().getDrawable(R.drawable.btn_pressed));
+        selectorButton.setStateDisabledDrawable(getResources().getDrawable(R.drawable.btn_disabled));
     }
 }
